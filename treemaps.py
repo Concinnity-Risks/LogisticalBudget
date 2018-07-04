6b63551882d5fe3ea0422762ee48e57b394ba883
erinb52s <erinb52s@gmail.com>
2018-07-04 13:14:52 +0100

    added treemap

---
+++ treemaps.py
@@ -0,0 +1,357 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Standard Python imports
+#
+import sys
+import datetime
+import math
+import subprocess
+
+# For progress bars
+from tqdm import tqdm
+
+# For tree map plotting
+#
+import plotly
+# OR import plotly.plotly as py
+import plotly.graph_objs as graph_objs
+# OR import plotly.graph_objs as go
+
+import squarify
+
+# Modules directly associated with this application
+#
+import scoring
+import utility
+
+
+def generate_treemaps(misp_data, num_days, bin_size, bin_name):
+    """
+    Generate treemaps for various criteria
+
+    misp_data - The events and attributes loaded from the MISP server
+    num_days - The number of days to graph
+    bin_size - The number of days in each graph block
+    bin_name - The name for a graph block
+    """
+
+    x = 0.
+    y = 0.
+    width = 100.
+    height = 100.
+
+    sets = []
+
+    normed = squarify.normalize_sizes(values, width, height)
+    rects = squarify.squarify(normed, x, y, width, height)
+
+    # Choose colors from http://colorbrewer2.org/ under "Export"
+    color_brewer = ['rgb(166,206,227)','rgb(31,120,180)','rgb(178,223,138)',
+                    'rgb(51,160,44)','rgb(251,154,153)','rgb(227,26,28)']
+    shapes = []
+    annotations = []
+    counter = 0
+
+    for html in [True, False]:
+        sets.append(
+        dict(
+            type = 'rect',
+            x0 = r['x'],
+            y0 = r['y'],
+            x1 = r['x']+r['dx'],
+            y1 = r['y']+r['dy'],
+            line = dict( width = 2 ),
+            fillcolor = color_brewer[counter]
+        )
+    )
+        sets.append(
+            {"num_days": num_days,
+             "bin_size": bin_size,
+             "scoring_function": scoring.score_by_event_count,
+             "scoring_name": "Threat actor events",
+             "filename": "treemaps/treemap-count-" + bin_name,
+             "use_plotly": html
+             })
+        sets.append(
+            {"num_days": num_days,
+             "bin_size": bin_size,
+             "scoring_function": scoring.score_by_event_threat_level,
+             "scoring_name": "Sum of event threat levels (high = 100, medium = 50, low = 1)",
+             "filename": "treemaps/treemap-levels-" + bin_name,
+             "use_plotly": html
+             })
+        sets.append(
+            {"num_days": num_days,
+             "bin_size": bin_size,
+             "scoring_function": scoring.score_by_source_ips,
+             "scoring_name": "Number of source IP addresses implicated",
+             "filename": "treemaps/treemap-ipsrc-" + bin_name,
+             "use_plotly": html
+             })
+        sets.append(
+            {"num_days": num_days,
+             "bin_size": bin_size,
+             "scoring_function": scoring.score_by_destination_ips,
+             "scoring_name": "Number of destination IP addresses implicated",
+             "filename": "treemaps/treemap-ipdst-" + bin_name,
+             "use_plotly": html
+             })
+        sets.append(
+            {"num_days": num_days,
+             "bin_size": bin_size,
+             "scoring_function": scoring.score_by_domain_count,
+             "scoring_name": "Number of domains implicated",
+             "filename": "treemaps/treemap-domains-" + bin_name,
+             "use_plotly": html
+             })
+        sets.append(
+            {"num_days": num_days,
+             "bin_size": bin_size,
+             "scoring_function": scoring.score_by_malware_files,
+             "scoring_name": "Numbers of malware files recorded",
+             "filename": "treemaps/treemap-files-" + bin_name,
+             "use_plotly": html
+             })
+        sets.append(
+            {"num_days": num_days,
+             "bin_size": bin_size,
+             "scoring_function": scoring.score_by_amount_of_external_analysis,
+             "scoring_name": "Amount of external analysis recorded",
+             "filename": "treemaps/treemap-analysis-" + bin_name,
+             "use_plotly": html
+             })
+
+    for set in sets:
+        generate_by_threat_actor(misp_data, **set)
+
+
+def generate_by_threat_actor(misp_data, num_days, bin_size, scoring_function, scoring_name, filename, use_plotly):
+    """
+    Generate a tree map for each threat actor, and save either as HTML or as a gnuplot input file and PNG
+
+    misp_data - The events and attributes loaded from the MISP server
+    num_days - The total number of days to generate the tree map for
+    bin_size - The number of days to bin events into (e.g. 7 to put them into weekly bins)
+    scoring_function - Function to score the event
+    scoring_name - A string to describe the scoring of the event
+    filename - The name of the file to write the treemap to
+    use_plotly - True to use plotly for the rendering, False to output data for gnuplot
+    """
+
+    events = misp_data["events"]
+    attributes = misp_data["attributes"]
+
+    title = scoring_name + " per " + str(bin_size) + " day period"
+    print("Generating " + ("HTML" if use_plotly else "PNG") + " for " + title)
+
+    time_base = datetime.datetime.today()
+    unattributed = "Unattributed"
+
+    # Generate dictionary of threat actors
+    threat_actors = utility.identify_threat_actors(misp_data, initial={unattributed: True})
+
+    # Construct an initial table of actors and number of events by day
+    #
+    rows = {}
+    for actor in threat_actors.keys():
+        rows[actor] = []
+        for d in range(math.ceil(num_days / bin_size)):
+            rows[actor].append(0)
+
+    # Scan the events by actor and timestamp
+    #
+    for event in tqdm(events):
+        event_id = int(event["id"])
+        if event_id in attributes:
+            event_attributes = attributes[event_id]
+        else:
+            event_attributes = []
+
+        event_actor = unattributed
+
+        if "GalaxyCluster" in event:
+            galaxycluster = event["GalaxyCluster"]
+            for galaxy in galaxycluster:
+                if "Galaxy" in galaxy:
+                    if galaxy["type"] == "threat-actor":
+                        event_actor = galaxy["value"]
+
+        if "timestamp" in event:
+            seconds_since_epoch = int(event["timestamp"])
+            if seconds_since_epoch > 1:
+                event_time = datetime.datetime.fromtimestamp(
+                    seconds_since_epoch)
+                event_age = time_base - event_time
+                if event_age.days < num_days:
+                    score = scoring_function(event, event_attributes)
+                    rows[event_actor][int(event_age.days / bin_size)] += score
+
+    # Remove the unattributed row, as it dwarfs data from the attributed events
+    threat_actors.pop(unattributed, None)
+
+    # Remove all actors with no activity logged
+    #
+    actors_to_remove = []
+
+    for actor in threat_actors.keys():
+        some_activity = False
+        for freq in rows[actor]:
+            if freq != 0:
+                some_activity = True
+        if not some_activity:
+            actors_to_remove.append(actor)
+
+    for actor in actors_to_remove:
+        threat_actors.pop(actor, None)
+
+    # Sort the threat actors by aggregate score, so that the most active
+    # are at the bottom of the chart
+    #
+    sorted_threat_actors = []
+    sorted_rows = []
+    for key, value in sorted(threat_actors.items(), key=lambda x: sum(rows[x[0]])):
+        sorted_threat_actors.append(key)
+        sorted_rows.append(rows[key])
+
+    # Now plot the graph
+    #
+    if use_plotly:
+        date_list = [time_base - datetime.timedelta(days=x)
+                     for x in range(0, num_days, bin_size)]
+
+        frequency_data = []
+        for row in sorted_rows:
+            frequency_data.append(list(row))
+
+        data = [
+            graph_objs.Treemap(
+                z=frequency_data,
+                x=date_list,
+                y=sorted_threat_actors,
+                colorscale="Hot",
+            )
+        ]
+
+        layout = graph_objs.Layout(
+            title=title,
+            xaxis=dict(ticks="", nticks=36),
+            yaxis=dict(ticks="")
+        )
+
+        fig = graph_objs.Figure(data=data, layout=layout)
+        plotly.offline.plot(fig, filename=filename + ".html",
+                            auto_open=False, show_link=False)
+    else:
+        if not threat_actors:
+            print("No threat actors")
+        else:
+            height = len(sorted_threat_actors)
+            width = len(sorted_rows[0])
+
+            minz = 1.0e9
+            maxz = -1.0e9
+            for row in sorted_rows:
+                for datum in row:
+                    if datum < minz:
+                        minz = datum
+                    if datum > maxz:
+                        maxz = datum
+
+            with open(filename + ".plt", "w") as outfile:
+                # Set the size of the output image
+                outfile.write("set terminal png size 1280, 1024\n")
+
+                # Set the filename of the output image
+                outfile.write("set output \"" + filename + ".png\"\n")
+
+                # Set the title of the graph
+                outfile.write("set title \"" + title + "\"\n")
+
+                # Label the score map
+                outfile.write("set cblabel \"" + scoring_name + "\"\n")
+
+                # Don't draw a key because this is going to be a treemap
+                outfile.write("unset key\n")
+
+                # Produce a 2D treemap, not a 3D surface plot
+                #
+                outfile.write("set view map scale 1\n")
+                outfile.write("set style data lines\n")
+
+                # Turn off tic marks
+                outfile.write("set tics scale 0\n")
+
+                # Set the left margin so that threat actor names are not truncated
+                outfile.write("set lmargin 15\n")
+
+                # Specify the labels for the X-axis, i.e. the dates
+                #
+                outfile.write("set xtics (")
+                every_n_bins = 2
+                i = 0
+                for days in range(0, num_days, bin_size * every_n_bins):
+                    stamp = time_base - \
+                        datetime.timedelta(days=(num_days - days))
+                    date = str(stamp.day) + "/" + \
+                        str(stamp.month) + "/" + str(stamp.year)
+                    outfile.write("\"" + date + "\" " + str(i) + ", ")
+                    i += every_n_bins
+                outfile.write(")\n")
+
+                # Specify the labels for the Y-axis, i.e. the threat actors
+                #
+                outfile.write("set ytics (")
+                i = 0
+                for actor in sorted_threat_actors:
+                    outfile.write("\"" + actor + "\" " + str(i) + ", ")
+                    i += 1
+                outfile.write(")\n")
+
+                # Set the ranges of the data that will be plot
+                #
+                outfile.write(
+                    "set xrange [ -0.5 : " + str(width - 0.5) + " ]\n")
+                outfile.write(
+                    "set yrange [ -0.5 : " + str(height - 0.5) + " ]\n")
+                outfile.write(
+                    "set zrange [ " + str(minz) + " : " + str(maxz) + " ]\n")
+                outfile.write(
+                    "set cbrange [ " + str(minz) + " : " + str(maxz) + " ]\n")
+
+                # Set the treemap palette
+                #
+                outfile.write(
+                    "set palette defined (0 \"black\", 0.33 \"red\", 0.66 \"yellow\", 1 \"white\")\n")
+
+                # Now write out the data for the treemap
+                #
+                outfile.write("$map1 << EOD\n")
+
+                for row in sorted_rows:
+                    for datum in reversed(row):
+                        outfile.write(str(datum) + " ")
+                    outfile.write("\n")
+
+                # gnuplot seems to want an extra row of data for some reason.
+                # I have a suspicion that this is something to do with interpolation
+                # of values, if you happened to set that -- but we don't.
+                #
+                for datum in range(width):
+                    outfile.write("0 ")
+                outfile.write("\n")
+
+                # End the data and plot the treemap
+                #
+                outfile.write("EOD\n")
+                outfile.write("splot \"$map1\" matrix with image\n")
+
+            try:
+                process = subprocess.Popen(
+                    args=["gnuplot", filename + ".plt"],
+                    stdout=subprocess.PIPE)
+                output = process.communicate()[0].decode("utf-8")
+                if len(output) != 0:
+                    print(output)
+            except Exception as e:
+                print("Unable to run gnuplot: Is it installed?  " + repr(e))
